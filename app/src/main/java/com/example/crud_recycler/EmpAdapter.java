eccbd775e3def84e943525f59cd7cb689e7c032b
Mujtaba540 <Mujtabakhalid1999@gmail.com>
2021-12-24 18:42:42 +0500

    Recycler view added implimented

--- app/src/main/java/com/example/crud_recycler/EmpAdapter.java
+++ app/src/main/java/com/example/crud_recycler/EmpAdapter.java
@@ -1,5 +1,6 @@
 package com.example.crud_recycler;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -8,33 +9,46 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class EmpAdapter extends RecyclerView.Adapter<recyclerAdapter.MyViewHolder> {
+import java.util.ArrayList;
+
+public class EmpAdapter extends RecyclerView.Adapter<EmpAdapter.MyViewHolder> {
     @NonNull
+    private ArrayList<Employee> data;
+    public EmpAdapter(ArrayList<Employee> data){
+        this.data=data;
+    }
+
+
+
+
     @Override
-    public recyclerAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView= LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item,parent,false);
+        return new MyViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull recyclerAdapter.MyViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        String name=data.get(position).getName();
+        holder.emp_name.setText(name);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.size();
     }
 
-    class MyViewHolder extends RecyclerView.ViewHolder{
-        private TextView emp_name,emp_loc,empl_des;
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        private TextView emp_name,emp_loc,emp_des;
         private Button upt_btn,dlt_btn;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             emp_name=itemView.findViewById(R.id.emp_name);
             emp_loc=itemView.findViewById(R.id.emp_loc);
-            empl_des=itemView.findViewById(R.id.emp_des);
+            emp_des=itemView.findViewById(R.id.emp_des);
             upt_btn=itemView.findViewById(R.id.update);
             dlt_btn=itemView.findViewById(R.id.dlt);
         }
     }
+
 }
--- app/src/main/java/com/example/crud_recycler/MainActivity.java
+++ app/src/main/java/com/example/crud_recycler/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.crud_recycler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    ArrayList<Employee> data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView=findViewById(R.id.myrec);
+        data=new ArrayList<>();
+
+        setEmployeeInfo();
+        setAdapter();
     }
+
+    private void setAdapter(){
+        EmpAdapter adapter=new EmpAdapter(data);
+        RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void setEmployeeInfo(){
+        data.add(new Employee("mujtbata","lahore","123"));
+        data.add(new Employee("mujtbata","lahore","123"));
+    }
+
 }
